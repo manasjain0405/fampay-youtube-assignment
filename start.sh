16e4be49abc042dfd6a244f9edec0974bc3ade68
manasjain0405 <manas.jain@groww.in>
2022-07-10 09:24:06 +0530

    docker config init

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-java $JAVA_OPTS -jar onboarding-data-server.jar $GROW_OPTS -Xms$JAVA_PROCESS_MIN_HEAP -Xmx$JAVA_PROCESS_MAX_HEAP --spring.profiles.active=$SPRING_ACTIVE_PROFILE
+docker build -t video-feed-application:1.0 ./video-feed-interface/
+docker build -t video-scraper-application:1.0 ./video-scraper/
+docker compose up -d
