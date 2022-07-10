9eb00e28cfcee5cda848c6a0fd64f4d34ca86fe3
manasjain0405 <manas.jain@groww.in>
2022-07-10 15:37:42 +0530

    Optimisations

--- video-scraper/src/main/java/in/fampay/videoscraper/service/impl/YoutubeVideoFetchServiceImpl.java
+++ video-scraper/src/main/java/in/fampay/videoscraper/service/impl/YoutubeVideoFetchServiceImpl.java
@@ -5,13 +5,14 @@ import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.Optional;
 import java.util.Stack;
+import javax.annotation.PostConstruct;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException.FeignClientException;
 import in.fampay.videoscraper.client.YoutubeApiClient;
 import in.fampay.videoscraper.constant.StreamBridgeConstants;
 import in.fampay.videoscraper.constant.YoutubeApiConstants;
 import in.fampay.videoscraper.dto.youtubeapi.YoutubeSearchResponseDto;
-import in.fampay.videoscraper.exceptions.KeyExpiredException;
 import in.fampay.videoscraper.exceptions.OutOfApiKeyException;
 import in.fampay.videoscraper.mappers.YoutubeSearchMapper;
 import in.fampay.videoscraper.service.VideoFetchService;
@@ -22,7 +23,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service("youtube-video-fetch-service")
@@ -31,7 +31,7 @@ public class YoutubeVideoFetchServiceImpl implements VideoFetchService {
 
   private final YoutubeApiClient apiClient;
   private final Stack<String> keyStack;
-
+  private final String[] keyList;
   private final StreamBridge streamBridge;
   private final YoutubeSearchMapper youtubeSearchMapper;
   private final ObjectMapper objectMapper;
@@ -46,38 +46,46 @@ public class YoutubeVideoFetchServiceImpl implements VideoFetchService {
     this.apiClient = apiClient;
     this.streamBridge = streamBridge;
     this.keyStack = new Stack<>();
-    keyStack.addAll(Arrays.asList(keyList));
+    this.keyList = keyList;
     this.objectMapper = objectMapper;
     this.youtubeSearchMapper = youtubeSearchMapper;
   }
 
+  @PostConstruct
+  void fillStack() {
+
+    log.info("Keys: {}", Arrays.asList(this.keyList));
+    this.keyStack.addAll(Arrays.asList(this.keyList));
+  }
+
   @Override
-  @Retryable(maxAttempts = 2, value = KeyExpiredException.class)
   public void fetchVideoOnQueryParam(final String query, final Long recordFetchCount) {
 
     if (keyStack.isEmpty()) {
       throw new OutOfApiKeyException("No more Api Keys Left");
     }
-    val key = keyStack.peek();
-    val result = apiClient.fetchYoutubeSearchResultPaginated(key, query,
-        YoutubeApiConstants.PART_PARAMETER, recordFetchCount,
-        YoutubeApiConstants.ORDER_PARAMETER, YoutubeApiConstants.TYPE_PARAMETER,
-        ZonedDateTime.now(ZoneId.of("Asia/Kolkata")).minusDays(60));
-    if (YoutubeApiConstants.API_SUCCESS_CODE.contains(result.getStatusCodeValue())) {
-      Optional.ofNullable(result.getBody())
-          .map(YoutubeSearchResponseDto::getYoutubeSearchItems)
-          .ifPresent(youtubeSearchItems ->
-              youtubeSearchItems.parallelStream()
-                  .map(youtubeSearchMapper::convertYoutubeSearchDtoToEntity)
-                  .forEach(ThrowingConsumer.unchecked(x -> {
-                    streamBridge.send(StreamBridgeConstants.INIT_VIDEO_REFRESH_TOPIC,
-                        MessageBuilder.withPayload(objectMapper.writeValueAsString(x))
-                            .setHeader(KafkaHeaders.MESSAGE_KEY, x.getReferenceId())
-                            .build());
-                  })));
-    } else if (YoutubeApiConstants.API_KEY_EXPIRE_CODE == result.getStatusCodeValue()) {
-      log.info("Key Expired Popping Out Of Stack {}", keyStack.pop());
-      throw new KeyExpiredException("Key Expired");
+    try {
+      val result = apiClient.fetchYoutubeSearchResultPaginated(this.keyStack.peek(),
+          query, YoutubeApiConstants.PART_PARAMETER, recordFetchCount,
+          YoutubeApiConstants.ORDER_PARAMETER, YoutubeApiConstants.TYPE_PARAMETER,
+          ZonedDateTime.now(ZoneId.of("Asia/Kolkata")).minusDays(60));
+      if (YoutubeApiConstants.API_SUCCESS_CODE.contains(result.getStatusCodeValue())) {
+        Optional.ofNullable(result.getBody())
+            .map(YoutubeSearchResponseDto::getYoutubeSearchItems)
+            .ifPresent(youtubeSearchItems ->
+                youtubeSearchItems.parallelStream()
+                    .map(youtubeSearchMapper::convertYoutubeSearchDtoToEntity)
+                    .forEach(ThrowingConsumer.unchecked(x -> {
+                      streamBridge.send(StreamBridgeConstants.INIT_VIDEO_REFRESH_TOPIC,
+                          MessageBuilder.withPayload(objectMapper.writeValueAsString(x))
+                              .setHeader(KafkaHeaders.MESSAGE_KEY, x.getReferenceId())
+                              .build());
+                    })));
+      }
+    } catch (FeignClientException.Forbidden e) {
+      log.info("Key Expired Popping Out Of Stack {}", this.keyStack.pop());
+    } catch (Exception e) {
+      log.info("Exception occurred while fetching details", e);
     }
   }
 }
