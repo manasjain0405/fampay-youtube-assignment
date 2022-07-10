c3d54baebc475d4bfe0ee6c8908590d78f54be04
manasjain0405 <manas.jain@groww.in>
2022-07-10 14:05:13 +0530

    Docker Config

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM adoptopenjdk:11-jre-hotspot
-EXPOSE 8020
-ADD target/video-scraper.jar video-scraper.jar
-ENTRYPOINT ["java", "-jar", "application.jar"]
--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
-docker build -t video-feed-application:1.0 ./video-feed-interface/
-docker build -t video-scraper-application:1.0 ./video-scraper/
+mvn clean install -f ./video-scraper/pom.xml > video-scraper-build.log
+mvn clean install -f ./video-feed-interface/pom.xml > video-feed-build.log
+docker build . -t video-scraper-application:1.0 -f video-scraper/Dockerfile
+docker build . -t video-feed-application:1.0 -f video-feed-interface/Dockerfile
 docker compose up -d
